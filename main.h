1bff2a146cba61b8e049ddeab07e275d6d234155
Jetsada Machom <zinuzoid@gmail.com>
2010-12-16 17:49:43 +0700

    add calc stack, not working yet

---
+++ main.h
@@ -0,0 +1,17 @@
+	
+	
+#define	WELCOMETEXT1	"      ___          _                  _           _                \r\n"
+#define	WELCOMETEXT2	"     (  _`\\       (_ )               (_ )        ( )_              \r\n"
+#define	WELCOMETEXT3	" ____| ( (_)   _ _ | |    ___  _   _  | |    _ _ | ,_)   _    _ __ \r\n"
+#define	WELCOMETEXT4	"(_  ,) |  _  /'_` )| |  /'___)( ) ( ) | |  /'_` )| |   /'_`\\ ( '__)\r\n"
+#define	WELCOMETEXT5	" /'/_| (_( )( (_| || | ( (___ | (_) | | | ( (_| || |_ ( (_) )| |   \r\n"
+#define	WELCOMETEXT6	"(____)____/'`\\__,_)___)`\\____)`\\___/'(___)`\\__,_)`\\__)`\\___/'(_)   \r\n"
+
+
+#define	WELCOMETEXT7	"   ____                            \r\n"
+#define	WELCOMETEXT8	"  /_  ,'()_        ____  _   () _//\r\n"
+#define	WELCOMETEXT9	"  ,',' /7/ \\/7/7/7/_ ,','o| /7,'o/ \r\n"
+#define	WELCOMETEXT10	",'___////_n_//__/ ,'__/|_,'//|__/  \r\n"
+#define	WELCOMETEXT11	"                                   \r\n"
+
+#define	COMMANDPROMPT	">> "
